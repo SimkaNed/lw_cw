12324950313d223d646fda24af300792f1f82ace
Simeon <simkaned@gmail.com>
2022-01-27 00:32:25 +0300

    begin to implement control unit interface

--- generator/controller_interface_test.py
+++ generator/controller_interface_test.py
@@ -1,11 +1,11 @@
 
 
-# control unit interface with LCM 
-# 
+# control unit interface with LCM
+#
 # ////////////////
-# ///// LCM ////// 
+# ///// LCM //////
 # ////////////////
-# 
+#
 # CONTROLLER_STATE (PUBLISH)
 #     int64_t  timestamp;
 #     double   state[STATE_DIM];
@@ -14,68 +14,114 @@
 #     double   input[INPUT_DIM];
 #     string   plant_name;
 #     bool     active;
-#  
+#
 # USER_COMMANDS (PARSE)
-#     int64_t timestamp; 
+#     int64_t timestamp;
 #     int16_t  mode;
 #     double   measurements[MEAS_DIM];
 #     int32_t error_code;
 #     // The commands to add
 
 # PLANT_STATE (PARSE)
-    # const int16_t STATE_DIM=2, OUTPUT_DIM = 1, INPUT_DIM=1;
-    # int64_t timestamp; 
-    # int16_t  mode;
-    # double   measurements[MEAS_DIM];
-    # int32_t error_code;
-    # // The commands to add
+# const int16_t STATE_DIM=2, OUTPUT_DIM = 1, INPUT_DIM=1;
+# int64_t timestamp;
+# int16_t  mode;
+# double   measurements[MEAS_DIM];
+# int32_t error_code;
+# // The commands to add
 
 # //////////////////////
 # /// SHARED MEMORY ////
-# //////////////////////  
-# 
+# //////////////////////
+#
 #  CONTROL_UNIT STATES
 #    - desired_state
 #    - reference_state  (if planner)
 #    - reference_control (if planner)
 #    - estimated_states
-#    - control 
+#    - control
 #    - output
-# 
+#
 
 import lcm
 from time import perf_counter
-from protocol import state_t, command_t
+from system.protocol import user_commands_t, controller_states_t, plant_states_t
 from multiprocessing import Process, Manager
+from system.parameters import STATE_DIM, OUTPUT_DIM, INPUT_DIM, MEASUREMENTS_DIM, USER_INPUT_DIM
 
 
-# TODO: Add address
+# TODO:
+#  - add address
+#  - store shared states not in Namespace but Arrays
 
 class ControlUnit:
-    def __init__(self) -> None:
-        self.lcm_node = lcm.LCM()
+    def __init__(self, address=None) -> None:
 
+        # INITIALIZE SHARED MEMORY 
         self.control_unit = Manager().Namespace()
-        
-        self.control_unit.desired_state = 0 
-        self.control_unit.reference_state = 0 
-        self.control_unit.reference_control = 0 
-        self.control_unit.estimated_states = 0 
-        self.control_unit.control = 0 
-        self.control_unit.output = 0 
-        
+
+        self.control_unit.desired_state = 0
+        self.control_unit.reference_state = 0
+        self.control_unit.reference_control = 0
+        self.control_unit.estimated_states = 0
+        self.control_unit.control = 0
+        self.control_unit.output = 0
+
         self.user_commands = Manager().Namespace()
+        self.user_commands.desired_postion = 0
+        
 
-    def handle_lcm(self):
-        pass
+        # /////// SETLING LCM ///////
 
-    def send_control(self):
-        pass
+        self.address = address
+        # Initialize LCM
+        self.lcm_node = lcm.LCM(self.address)
+        # Define channels
+        self.user_channel = "user"
+        self.controller_channel = "controller"
+        self.plant_channel = "plant"
+        # Initiate the messages
+        self.controller_message = controller_states_t()
+        self.plant_message = plant_states_t()
+        self.user_message = user_commands_t()
+
+        # self.subscriptions = []
+
+        # self.command_message = command_t()
+
+    # def sta
 
     def update_user_command(self, channel, data):
         pass
 
+    def update_plant(self, channel, data):
+        pass
+
     def update_output(self, channel, data):
+        pass
+
+    def subscribe(self):
+        """Subscribe to plant and user commands"""
+        self.plant_subscription = self.subscribe(self.plant_channel, self.update_plant)
+        self.user_subscription = self.subscribe(self.user_channel, self.update_user_command)
+    
+    def publish(self):
+        """publish the controller states to LCM"""
+        self.controller_message.state_estimates = self.control_unit.estimated_states
+        self.controller_message.output = self.control_unit.output
+        self.controller_message.input = self.control_unit.control
+
+        self.lc.publish(self.controller_channel, self.controller_message.encode())
+           
+    def handle_lcm(self):
+        """Handle LCM subscriptions"""
+        self.lc.handle()
+        # pass
+
+    def set_states(self):
+        pass
+
+    def set_control(self):
         pass
 
     def get_user_command(self):
@@ -83,10 +129,9 @@ class ControlUnit:
 
     def get_plant_state(self):
         pass
-    
-    def set_control(self):
-        pass 
 
+    def set_control(self):
+        pass
 
     # handle()
 
@@ -149,7 +194,7 @@ class ControlUnit:
 #         self._process = Process(target=self.lcm_process)
 #         self._shared = Manager().Namespace()
 #         self._shared.is_active = False
-        
+
 #     def __del__(self) -> None:
 #         print('deleted')
 
@@ -162,7 +207,7 @@ class ControlUnit:
 #                 self.lcm.update_state()
 
 #     def start(self):
-#         self._process.start() 
+#         self._process.start()
 #         print('started')
 
 #     def pause(self):
@@ -172,7 +217,7 @@ class ControlUnit:
 #     def resume(self):
 #         print('resumed')
 #         self._shared.is_active = True
-        
+
 
 #     def stop(self):
 #         self._process.terminate()
